87cd913363c54f5e8512bb6fceedcf62285e3620
fdi74 <dallignafabricio@gmail.com>
2026-08-30 11:02:01 -0300

    Add LiftDiamond route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Contato from "./pages/Contato";
-import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound";import LiftDiamond from "./pages/LiftDiamond";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/contato" element={<Contato />} />
+          <Route path="/contato" element={<Contato />} /><Route path="/lift-diamond" element={<LiftDiamond />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
